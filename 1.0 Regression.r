33d093ffb40c7cc300b5a0d91753e7a8da7a1ada
Dorcas Kareithi <19709060+DeeKareithi@users.noreply.github.com>
2021-11-15 20:09:53 +0000

    initial comit

---
+++ 1.0 Regression.r
@@ -0,0 +1,9 @@
+#Dorcas Kareithi
+#NOvember 2021
+#Task: Refreshing on epidemiology program evalution techniques.
+
+#Notes: I am following a code from https://evalf20.classes.andrewheiss.com/example/regression/. I have endevaoured to replicate all codes in stata. There is a stata version of the same, called 1.1s
+
+#Installing all packages used on this script (could spill over to other scripts)
+
+
