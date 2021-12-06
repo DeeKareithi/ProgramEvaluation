54a447c35f95a40d3a18fe03587f6a38af6d6cdd
Dorcas Kareithi <19709060+DeeKareithi@users.noreply.github.com>
2021-12-06 12:42:57 +0000

    Update 1.0 Regression.r

--- 1.0 Regression.r
+++ 1.0 Regression.r
@@ -1,5 +1,5 @@
 #Dorcas Kareithi
-#NOvember 2021
+#November 2021
 #Task: Refreshing on epidemiology program evalution techniques.
 
 #Notes: I am following a code from https://evalf20.classes.andrewheiss.com/example/regression/. I have endevaoured to replicate all codes in stata. There is a stata version of the same, called 1.1s
